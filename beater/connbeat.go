0a59df4696657c832fb8f39b3bb2cacba6a28e59
Arnout Engelen <arnouten@bzzt.net>
2016-05-27 15:23:40 +0200

    Publish local IP's as beat metadata

--- beater/connbeat.go
+++ beater/connbeat.go
@@ -3,6 +3,8 @@ package beater
 import (
 	"time"
 
+	"github.com/deckarep/golang-set"
+
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/publisher"
@@ -28,12 +30,15 @@ func (cb *Connbeat) Setup(b *beat.Beat) error {
 	return nil
 }
 
-func (cb *Connbeat) exportServerConnection(s ServerConnection) error {
+func (cb *Connbeat) exportServerConnection(s ServerConnection, localIps mapset.Set) error {
 	event := common.MapStr{
 		"@timestamp":    common.Time(time.Now()),
 		"type":          "connbeat",
 		"local_port":    s.localPort,
 		"local_process": s.process,
+		"beat": common.MapStr{
+			"local_ips": localIps.ToSlice(),
+		},
 	}
 
 	cb.events.PublishEvent(event)
@@ -41,7 +46,7 @@ func (cb *Connbeat) exportServerConnection(s ServerConnection) error {
 	return nil
 }
 
-func (cb *Connbeat) exportConnection(c Connection) error {
+func (cb *Connbeat) exportConnection(c Connection, localIps mapset.Set) error {
 	event := common.MapStr{
 		"@timestamp":    common.Time(time.Now()),
 		"type":          "connbeat",
@@ -50,6 +55,9 @@ func (cb *Connbeat) exportConnection(c Connection) error {
 		"remote_ip":     c.remoteIp,
 		"remote_port":   c.remotePort,
 		"local_process": c.process,
+		"beat": common.MapStr{
+			"local_ips": localIps.ToSlice(),
+		},
 	}
 
 	cb.events.PublishEvent(event)
@@ -57,23 +65,33 @@ func (cb *Connbeat) exportConnection(c Connection) error {
 	return nil
 }
 
-func (cb *Connbeat) Run(b *beat.Beat) error {
+func (cb *Connbeat) Pipe(connectionListener <-chan Connection, serverConnectionListener <-chan ServerConnection) error {
 	var err error
 
-	connectionListener, serverConnectionListener := Listen()
+	localIps := mapset.NewSet()
 
 	for {
 		select {
 		case <-cb.done:
 			return nil
 		case c := <-connectionListener:
-			err = cb.exportConnection(c)
+			localIps.Add(c.localIp)
+			err = cb.exportConnection(c, localIps)
 		case s := <-serverConnectionListener:
-			err = cb.exportServerConnection(s)
+			if s.localIp != "0.0.0.0" {
+				localIps.Add(s.localIp)
+			}
+			err = cb.exportServerConnection(s, localIps)
 		}
 	}
 
 	return err
+}
+
+func (cb *Connbeat) Run(b *beat.Beat) error {
+	connectionListener, serverConnectionListener := Listen()
+
+	return cb.Pipe(connectionListener, serverConnectionListener)
 }
 
 func (cb *Connbeat) Cleanup(b *beat.Beat) error {
