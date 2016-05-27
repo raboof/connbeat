ae7198d66e3bdc162af5a3a640a8e309a2290965
Arnout Engelen <arnouten@bzzt.net>
2016-05-27 11:34:24 +0200

    Test we don't report client connections to us

--- beater/connections_test.go
+++ beater/connections_test.go
@@ -9,20 +9,47 @@ import (
   "github.com/elastic/beats/packetbeat/procs"
 )
 
-func serverConnection(port uint16) *procs.SocketInfo {
+func listeningConnection(port uint16) *procs.SocketInfo {
    return &procs.SocketInfo { rand.Uint32(), rand.Uint32(), port, 0, uint16(rand.Int()), rand.Int63() }
 }
 
-func TestFilterServerSockets(t *testing.T) {
+func incomingConnection(localPort uint16) *procs.SocketInfo {
+   return &procs.SocketInfo { rand.Uint32(), rand.Uint32(), localPort, uint16(rand.Uint32()), uint16(rand.Int()), rand.Int63() }
+}
+
+func TestDeduplicateListeningSockets(t *testing.T) {
   input := make(chan *procs.SocketInfo, 0)
   connections, servers := make(chan Connection, 0), make(chan ServerConnection, 0)
 
   go filterAndPublish(input, connections, servers)
 
-  input <- serverConnection(80)
+  input <- listeningConnection(80)
   _, ok := <-servers
   assert.Equal(t, ok, true, "a server should be reported")
-  input <- serverConnection(80)
+  input <- listeningConnection(80)
+
+  time.Sleep(100 * time.Millisecond)
+
+  select {
+  case <-connections:
+      t.Fail()
+  case <-servers:
+      t.Fail()
+    default:
+      // Nothing to read: OK!
+  }
+}
+
+func TestFilterConnectionsAssociatedWithListeningSockets(t *testing.T) {
+  input := make(chan *procs.SocketInfo, 0)
+  connections, servers := make(chan Connection, 0), make(chan ServerConnection, 0)
+
+  go filterAndPublish(input, connections, servers)
+
+  input <- listeningConnection(80)
+  _, ok := <-servers
+  assert.Equal(t, ok, true, "a server should be reported")
+  input <- incomingConnection(80)
 
   time.Sleep(100 * time.Millisecond)
 
