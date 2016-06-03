df8c276c047eac63196644da6e30ebc791ce8b67
Arnout Engelen <arnouten@bzzt.net>
2016-06-03 17:48:31 +0200

    Fix build

--- beater/connbeat.go
+++ beater/connbeat.go
@@ -109,11 +109,17 @@ func (cb *Connbeat) Pipe(connectionListener <-chan Connection, serverConnectionL
 		case c := <-connectionListener:
 			localIps.Add(c.localIp)
 			err = cb.exportConnection(c, localIps)
+			if err != nil {
+				return err
+			}
 		case s := <-serverConnectionListener:
 			if s.localIp != "0.0.0.0" {
 				localIps.Add(s.localIp)
 			}
 			err = cb.exportServerConnection(s, localIps)
+			if err != nil {
+				return err
+			}
 		}
 	}
 }
