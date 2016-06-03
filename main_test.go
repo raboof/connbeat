e6ac7a052bab52054e9f67c49cb5a2362db5e562
Arnout Engelen <arnouten@bzzt.net>
2016-06-03 17:39:03 +0200

    go fmt

--- main_test.go
+++ main_test.go
@@ -10,13 +10,13 @@ import (
 var systemTest *bool
 
 func init() {
-       systemTest = flag.Bool("systemTest", false, "Set to true when running system tests")
+	systemTest = flag.Bool("systemTest", false, "Set to true when running system tests")
 }
 
 // Test started when the test binary is started. Only calls main.
 func TestSystem(t *testing.T) {
 
-       if *systemTest {
-               main()
-       }
+	if *systemTest {
+		main()
+	}
 }
