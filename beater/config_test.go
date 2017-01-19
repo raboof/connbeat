e128d109c6fca625648d3d65fd7c4bd5c710936a
Arnout Engelen <arnouten@bzzt.net>
2017-01-19 13:45:16 +0100

    Formatting

--- beater/config_test.go
+++ beater/config_test.go
@@ -1,21 +1,21 @@
 package beater
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/stvp/assert"
+	"github.com/stvp/assert"
 )
 
 func TestErrorOnConflictingOptions(t *testing.T) {
-  config := ConnConfig{
-		DockerEnabled:         true,
-		TcpDiagEnabled:        true,
+	config := ConnConfig{
+		DockerEnabled:  true,
+		TcpDiagEnabled: true,
 	}
-  err := config.Validate()
-  assert.NotNil(t, err, "should produce an error when enabling both tcp_diag and docker")
+	err := config.Validate()
+	assert.NotNil(t, err, "should produce an error when enabling both tcp_diag and docker")
 }
 
 func TestNoErrorForDefaultConfig(t *testing.T) {
-  err := defaultConfig.Validate()
-  assert.Nil(t, err, "should not produce an error for the default options ")
+	err := defaultConfig.Validate()
+	assert.Nil(t, err, "should not produce an error for the default options ")
 }
