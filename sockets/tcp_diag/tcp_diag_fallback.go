fb8cab879d3e88f594b5eab546fcca3090879ae9
Arnout Engelen <arnouten@bzzt.net>
2017-03-30 09:42:53 +0200

    gofmt

--- sockets/tcp_diag/tcp_diag_fallback.go
+++ sockets/tcp_diag/tcp_diag_fallback.go
@@ -4,11 +4,11 @@ package tcp_diag
 
 import (
 	"errors"
-  "time"
+	"time"
 
-  "github.com/raboof/connbeat/sockets"
+	"github.com/raboof/connbeat/sockets"
 )
 
 func GetSocketInfo(pollInterval time.Duration, socketInfo chan<- *sockets.SocketInfo) error {
-  return errors.New("tcp_diag is only supported on linux")
+	return errors.New("tcp_diag is only supported on linux")
 }
