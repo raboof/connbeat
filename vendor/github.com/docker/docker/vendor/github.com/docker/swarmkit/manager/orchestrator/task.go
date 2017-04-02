69061c4c34a14c3f412cf70d9236894d2f4c0bf9
Glidebot <glidebot@bzzt.net>
2017-04-02 22:02:44 +0000

    Update glide dependencies

--- vendor/github.com/docker/docker/vendor/github.com/docker/swarmkit/manager/orchestrator/task.go
+++ vendor/github.com/docker/docker/vendor/github.com/docker/swarmkit/manager/orchestrator/task.go
@@ -9,10 +9,6 @@ import (
 	"github.com/docker/swarmkit/protobuf/ptypes"
 )
 
-// DefaultRestartDelay is the restart delay value to use when none is
-// specified.
-const DefaultRestartDelay = 5 * time.Second
-
 // NewTask creates a new task.
 func NewTask(cluster *api.Cluster, service *api.Service, slot uint64, nodeID string) *api.Task {
 	var logDriver *api.Driver
@@ -29,6 +25,7 @@ func NewTask(cluster *api.Cluster, service *api.Service, slot uint64, nodeID str
 		ID:                 taskID,
 		ServiceAnnotations: service.Spec.Annotations,
 		Spec:               service.Spec.Task,
+		SpecVersion:        service.SpecVersion,
 		ServiceID:          service.ID,
 		Slot:               slot,
 		Status: api.TaskStatus{
@@ -62,6 +59,13 @@ func RestartCondition(task *api.Task) api.RestartPolicy_RestartCondition {
 
 // IsTaskDirty determines whether a task matches the given service's spec.
 func IsTaskDirty(s *api.Service, t *api.Task) bool {
+	// If the spec version matches, we know the task is not dirty. However,
+	// if it does not match, that doesn't mean the task is dirty, since
+	// only a portion of the spec is included in the comparison.
+	if t.SpecVersion != nil && *s.SpecVersion == *t.SpecVersion {
+		return false
+	}
+
 	return !reflect.DeepEqual(s.Spec.Task, t.Spec) ||
 		(t.Endpoint != nil && !reflect.DeepEqual(s.Spec.Endpoint, t.Endpoint.Spec))
 }
--- vendor/github.com/docker/docker/vendor/github.com/docker/swarmkit/manager/orchestrator/update/updater.go
+++ vendor/github.com/docker/docker/vendor/github.com/docker/swarmkit/manager/orchestrator/update/updater.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/docker/go-events"
 	"github.com/docker/swarmkit/api"
+	"github.com/docker/swarmkit/api/defaults"
 	"github.com/docker/swarmkit/log"
 	"github.com/docker/swarmkit/manager/orchestrator"
 	"github.com/docker/swarmkit/manager/orchestrator/restart"
@@ -20,8 +21,6 @@ import (
 	"github.com/docker/swarmkit/watch"
 	gogotypes "github.com/gogo/protobuf/types"
 )
-
-const defaultMonitor = 5 * time.Second
 
 // Supervisor supervises a set of updates. It's responsible for keeping track of updates,
 // shutting them down and replacing them.
@@ -156,13 +155,12 @@ func (u *Updater) Run(ctx context.Context, slots []orchestrator.Slot) {
 		u.startUpdate(ctx, service.ID)
 	}
 
-	var (
-		parallelism            = 1
-		delay                  time.Duration
-		failureAction          = api.UpdateConfig_PAUSE
-		allowedFailureFraction = float32(0)
-		monitoringPeriod       = defaultMonitor
-	)
+	delay := defaults.Service.Update.Delay
+	parallelism := int(defaults.Service.Update.Parallelism)
+	failureAction := defaults.Service.Update.FailureAction
+	allowedFailureFraction := defaults.Service.Update.MaxFailureRatio
+	monitoringPeriod, _ := gogotypes.DurationFromProto(defaults.Service.Update.Monitor)
+	order := defaults.Service.Update.Order
 
 	updateConfig := service.Spec.Update
 	if service.UpdateStatus != nil && service.UpdateStatus.State == api.UpdateStatus_ROLLBACK_STARTED {
@@ -174,12 +172,13 @@ func (u *Updater) Run(ctx context.Context, slots []orchestrator.Slot) {
 		allowedFailureFraction = updateConfig.MaxFailureRatio
 		parallelism = int(updateConfig.Parallelism)
 		delay = updateConfig.Delay
+		order = updateConfig.Order
 
 		var err error
 		if updateConfig.Monitor != nil {
 			monitoringPeriod, err = gogotypes.DurationFromProto(updateConfig.Monitor)
 			if err != nil {
-				monitoringPeriod = defaultMonitor
+				monitoringPeriod, _ = gogotypes.DurationFromProto(defaults.Service.Update.Monitor)
 			}
 		}
 	}
@@ -196,7 +195,7 @@ func (u *Updater) Run(ctx context.Context, slots []orchestrator.Slot) {
 	wg.Add(parallelism)
 	for i := 0; i < parallelism; i++ {
 		go func() {
-			u.worker(ctx, slotQueue, delay)
+			u.worker(ctx, slotQueue, delay, order)
 			wg.Done()
 		}()
 	}
@@ -207,9 +206,9 @@ func (u *Updater) Run(ctx context.Context, slots []orchestrator.Slot) {
 		var cancelWatch func()
 		failedTaskWatch, cancelWatch = state.Watch(
 			u.store.WatchQueue(),
-			state.EventUpdateTask{
+			api.EventUpdateTask{
 				Task:   &api.Task{ServiceID: service.ID, Status: api.TaskStatus{State: api.TaskStateRunning}},
-				Checks: []state.TaskCheckFunc{state.TaskCheckServiceID, state.TaskCheckStateGreaterThan},
+				Checks: []api.TaskCheckFunc{api.TaskCheckServiceID, state.TaskCheckStateGreaterThan},
 			},
 		)
 		defer cancelWatch()
@@ -270,7 +269,7 @@ slotsLoop:
 				stopped = true
 				break slotsLoop
 			case ev := <-failedTaskWatch:
-				if failureTriggersAction(ev.(state.EventUpdateTask).Task) {
+				if failureTriggersAction(ev.(api.EventUpdateTask).Task) {
 					break slotsLoop
 				}
 			case slotQueue <- slot:
@@ -295,7 +294,7 @@ slotsLoop:
 			case <-doneMonitoring:
 				break monitorLoop
 			case ev := <-failedTaskWatch:
-				if failureTriggersAction(ev.(state.EventUpdateTask).Task) {
+				if failureTriggersAction(ev.(api.EventUpdateTask).Task) {
 					break monitorLoop
 				}
 			}
@@ -310,7 +309,7 @@ slotsLoop:
 	}
 }
 
-func (u *Updater) worker(ctx context.Context, queue <-chan orchestrator.Slot, delay time.Duration) {
+func (u *Updater) worker(ctx context.Context, queue <-chan orchestrator.Slot, delay time.Duration, order api.UpdateConfig_UpdateOrder) {
 	for slot := range queue {
 		// Do we have a task with the new spec in desired state = RUNNING?
 		// If so, all we have to do to complete the update is remove the
@@ -347,7 +346,7 @@ func (u *Updater) worker(ctx context.Context, queue <-chan orchestrator.Slot, de
 			}
 			updated.DesiredState = api.TaskStateReady
 
-			if err := u.updateTask(ctx, slot, updated); err != nil {
+			if err := u.updateTask(ctx, slot, updated, order); err != nil {
 				log.G(ctx).WithError(err).WithField("task.id", updated.ID).Error("update failed")
 			}
 		}
@@ -362,11 +361,11 @@ func (u *Updater) worker(ctx context.Context, queue <-chan orchestrator.Slot, de
 	}
 }
 
-func (u *Updater) updateTask(ctx context.Context, slot orchestrator.Slot, updated *api.Task) error {
+func (u *Updater) updateTask(ctx context.Context, slot orchestrator.Slot, updated *api.Task, order api.UpdateConfig_UpdateOrder) error {
 	// Kick off the watch before even creating the updated task. This is in order to avoid missing any event.
-	taskUpdates, cancel := state.Watch(u.watchQueue, state.EventUpdateTask{
+	taskUpdates, cancel := state.Watch(u.watchQueue, api.EventUpdateTask{
 		Task:   &api.Task{ID: updated.ID},
-		Checks: []state.TaskCheckFunc{state.TaskCheckID},
+		Checks: []api.TaskCheckFunc{api.TaskCheckID},
 	})
 	defer cancel()
 
@@ -377,15 +376,11 @@ func (u *Updater) updateTask(ctx context.Context, slot orchestrator.Slot, update
 	u.updatedTasks[updated.ID] = time.Time{}
 	u.updatedTasksMu.Unlock()
 
+	startThenStop := false
 	var delayStartCh <-chan struct{}
 	// Atomically create the updated task and bring down the old one.
 	_, err := u.store.Batch(func(batch *store.Batch) error {
-		oldTask, err := u.removeOldTasks(ctx, batch, slot)
-		if err != nil {
-			return err
-		}
-
-		err = batch.Update(func(tx store.Tx) error {
+		err := batch.Update(func(tx store.Tx) error {
 			if store.GetService(tx, updated.ServiceID) == nil {
 				return errors.New("service was deleted")
 			}
@@ -399,7 +394,16 @@ func (u *Updater) updateTask(ctx context.Context, slot orchestrator.Slot, update
 			return err
 		}
 
-		delayStartCh = u.restarts.DelayStart(ctx, nil, oldTask, updated.ID, 0, true)
+		if order == api.UpdateConfig_START_FIRST {
+			delayStartCh = u.restarts.DelayStart(ctx, nil, nil, updated.ID, 0, false)
+			startThenStop = true
+		} else {
+			oldTask, err := u.removeOldTasks(ctx, batch, slot)
+			if err != nil {
+				return err
+			}
+			delayStartCh = u.restarts.DelayStart(ctx, nil, oldTask, updated.ID, 0, true)
+		}
 
 		return nil
 
@@ -421,11 +425,22 @@ func (u *Updater) updateTask(ctx context.Context, slot orchestrator.Slot, update
 	for {
 		select {
 		case e := <-taskUpdates:
-			updated = e.(state.EventUpdateTask).Task
+			updated = e.(api.EventUpdateTask).Task
 			if updated.Status.State >= api.TaskStateRunning {
 				u.updatedTasksMu.Lock()
 				u.updatedTasks[updated.ID] = time.Now()
 				u.updatedTasksMu.Unlock()
+
+				if startThenStop {
+					_, err := u.store.Batch(func(batch *store.Batch) error {
+						_, err := u.removeOldTasks(ctx, batch, slot)
+						if err != nil {
+							log.G(ctx).WithError(err).WithField("task.id", updated.ID).Warning("failed to remove old task after starting replacement")
+						}
+						return nil
+					})
+					return err
+				}
 				return nil
 			}
 		case <-u.stopChan:
